033c4f7ede2811d8cde34dd94525fae100925bc7
Shaquille Shaw <shaquilleshaw@Shaquilles-MacBook-Pro.local>
2022-12-20 17:34:37 -0500

    react router try again

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Books from "./pages/Books";
-// import Home from "./pages/Home";
-// import Projects from "./pages/Projects";
-// import Blogs from "./pages/Blogs";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Books from "./pages/Books";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Blogs from "./pages/Blogs";
 
 export default function App() {
   return (
-    <div>dsdfsff</div>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Home />}/>
-    //     <Route path="blogs" element={<Blogs />} />
-    //     <Route path="projects" element={<Projects />} />
-    //     <Route path="books" element={<Books />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="books" element={<Books />} />
+      </Routes>
+    </BrowserRouter>
   );
 }

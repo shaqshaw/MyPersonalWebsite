18d514aff3807827e6b0fcc4e399376c97010a34
Shaquille Shaw <shaquilleshaw@Shaquilles-MacBook-Pro.local>
2022-12-20 15:28:50 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Books from "./pages/Books";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Blogs from "./pages/Blogs";
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Books from "./pages/Books";
+// import Home from "./pages/Home";
+// import Projects from "./pages/Projects";
+// import Blogs from "./pages/Blogs";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="blogs" element={<Blogs />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="books" element={<Books />} />
-      </Routes>
-    </BrowserRouter>
+    <div>hello world</div>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />}/>
+    //     <Route path="blogs" element={<Blogs />} />
+    //     <Route path="projects" element={<Projects />} />
+    //     <Route path="books" element={<Books />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }

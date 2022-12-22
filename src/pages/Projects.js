6cd4e3379f9ed089bd000718e7e9953d0062b7c5
Shaquille Shaw <shaquilleshaw@Shaquilles-MacBook-Pro.local>
2022-12-21 20:18:04 -0500

    kdhd

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import profile from '../profile_pic.jpg';
 import '../App.css';
 import NavMenu from '../components/NavMenu';
 
@@ -9,17 +13,50 @@ function Projects() {
         <div className="App">
             <NavMenu/>
             <h1 className="titles">Projects</h1>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="sucess">Visit Site</Button>
-                </Card.Body>
-            </Card>
+
+            <Container>
+                <Row>
+                    <Col xsm={4}>
+                        <Card>
+                            <Card.Img variant="top" src={profile} />
+                            <Card.Body>
+                                <Card.Title className="card-info">Card Title</Card.Title>
+                                <Card.Text className="card-info">
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="success">Visit Site</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={4}>
+                        <Card>
+                            <Card.Img variant="top" src={profile} />
+                            <Card.Body>
+                                <Card.Title className="card-info">Card Title</Card.Title>
+                                <Card.Text className="card-info">
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="success">Visit Site</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs={4}>
+                        <Card>
+                            <Card.Img variant="top" src={profile} />
+                            <Card.Body>
+                                <Card.Title className="card-info">Card Title</Card.Title>
+                                <Card.Text className="card-info">
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="success">Visit Site</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }

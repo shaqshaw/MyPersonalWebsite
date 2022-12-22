19f468946dd4877e2ce50a44a09ee141de8d9a8c
Shaquille Shaw <shaquilleshaw@Shaquilles-MacBook-Pro.local>
2022-12-21 19:45:25 -0500

    starting on projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import '../App.css';
 import NavMenu from '../components/NavMenu';
 
@@ -6,6 +8,18 @@ function Projects() {
     return (
         <div className="App">
             <NavMenu/>
+            <h1 className="titles">Projects</h1>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="sucess">Visit Site</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 }
